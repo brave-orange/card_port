852414702c42ee5c5bdba9a295a65b01925fe726
root <root@localhost.localdomain>
2018-10-19 19:57:13 +0800

    a

--- app/chuanze/controller/Recharge.php
+++ app/chuanze/controller/Recharge.php
@@ -8,10 +8,10 @@ use \think\Session;
 class Recharge{
     public function index(){
          if(Request::instance()->isGet()){
+            Session::set("userid",1); //测试用，记得删除！
             $userid = Session::get("userid");
 
             $balance = user_balance($userid);
-            dump($userid);
             dump($balance);
             //return view();
          }
--- app/common.php
+++ app/common.php
@@ -55,8 +55,6 @@ function SendMessage($tel,$message){
 function user_balance($userid){    //通过充值和消费计算用户余额
     $balance = array();
     $res = model("Card")->group('type')->field('type')->select();
-
-    dump(model("Card")->getLastSql());
     $type = array();
     foreach($res as $k=>$v){
         $type[] = $v; 
