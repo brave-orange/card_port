486c3bef7711c2cd16127a500d8ee27c541c4b5b
root <root@localhost.localdomain>
2018-10-19 19:55:09 +0800

    a

--- app/common.php
+++ app/common.php
@@ -54,8 +54,9 @@ function SendMessage($tel,$message){
 
 function user_balance($userid){    //通过充值和消费计算用户余额
     $balance = array();
-    $res = model("Card")->group('type')->field('type')->select()->getLastSql();
-    dump($res);
+    $res = model("Card")->group('type')->field('type')->select();
+
+    dump(model("Card")->getLastSql());
     $type = array();
     foreach($res as $k=>$v){
         $type[] = $v; 
