c555c43059eaff0f0cc34b73d985918cad2fcbc1
root <root@localhost.localdomain>
2018-10-19 20:24:40 +0800

    a

--- app/chuanze/controller/Recharge.php
+++ app/chuanze/controller/Recharge.php
@@ -14,3 +14,6 @@ class Recharge{
             $balance = user_balance($userid);
             return $this->assign(array("balance"=>$balance))->fetch();
             //return view();
+        }
+    }
+}
