214aa6efc1b7a4d21c2a676b64d10fb2a4d8c399
root <root@localhost.localdomain>
2018-10-19 20:26:42 +0800

    a

--- app/chuanze/controller/Recharge.php
+++ app/chuanze/controller/Recharge.php
@@ -4,15 +4,17 @@ namespace app\chuanze\controller;
 use \think\Request;
 use \think\Db;
 use \think\Session;
+use \think\Controller;
 
-class Recharge{
+class Recharge extends Controller{
     public function index(){
          if(Request::instance()->isGet()){
             Session::set("userid",1); //测试用，记得删除！
             $userid = Session::get("userid");
 
             $balance = user_balance($userid);
-            return $this->assign(array("balance"=>$balance))->fetch();
+            return $this->assign(array("balance"=>$balance))->fetch("index");
+
             //return view();
         }
     }
