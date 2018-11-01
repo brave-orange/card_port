8a0bdd0c7bc9346877fe4fea5b7f5ccd282a2edc
YongCheng-w <anjxwyc@outlook.com>
2018-11-01 10:13:39 +0800

    改为正式环境

--- app/common/HaoChong.php
+++ app/common/HaoChong.php
@@ -5,12 +5,12 @@
  */
 namespace app\common;
 class HaoChong{
-    //static public $userid = "10001955";   //正式
-    static public $userid = "10001001";  //wait...（测试id）
+    static public $userid = "10001955";   //正式
+   // static public $userid = "10001001";  //wait...（测试id）
     static public $productid = "";
     static public $num = 1;
-    //static public $key = "mxt7fjSDf7E55tG4AF3ZzpxpXAtdFjXz";    //正式
-    static public $key = "bb673528fe8082614f9c3a5bdf2703d7";    //wait... （测试key）
+    static public $key = "mxt7fjSDf7E55tG4AF3ZzpxpXAtdFjXz";    //正式
+    //static public $key = "bb673528fe8082614f9c3a5bdf2703d7";    //wait... （测试key）
     static public $back_url = "http://card.onmycard.com.cn/backapi";
     public function __construct(){
         $this->spordertime=date("Y-m-d H:i:s");
@@ -26,8 +26,8 @@ class HaoChong{
         $this->price = $value;
         $this->mobile = $phone;
         $this->sporderid = $sporderid;
-        //$this->url = "http://180.96.21.204:8082/onlinepay.do";    //正式
-        $this->url = "http://121.40.152.174:28096/onlinepay.do";   //测试
+        $this->url = "http://180.96.21.204:8082/onlinepay.do";    //正式
+        //$this->url = "http://121.40.152.174:28096/onlinepay.do";   //测试
         $params = array();
         $sign = $this->getsign();
         $params['sign'] = $sign;
@@ -44,8 +44,8 @@ class HaoChong{
         
     }
     public function getOrder($orderid){    //查询订单
-        //$this->url = "http://180.96.21.204:8082/searchpay.do";
-        $this->url = "http://121.40.152.174:28096/searchpay.do";
+        $this->url = "http://180.96.21.204:8082/searchpay.do";
+        //$this->url = "http://121.40.152.174:28096/searchpay.do";
         $params['userid'] = self::$userid;
         $params['sporderid'] = self::$orderid;
         $xml_res =  http($this->url,$params,"POST");
@@ -53,8 +53,8 @@ class HaoChong{
     }
 
     public function getBalance(){     //查询余额
-        //$this->url = "http://180.96.21.204:8082/searchbalance.do";
-        $this->url = "http://121.40.152.174:28096/searchbalance.do";
+        $this->url = "http://180.96.21.204:8082/searchbalance.do";
+        //$this->url = "http://121.40.152.174:28096/searchbalance.do";
         $sign = md5("userid=".self::$userid."&key=".self::$key);
         $params['userid'] = self::$userid;
         $params['sign'] = $sign;
