b35c31947f2dea69240fb64e615fc96a0614ad2f
renluyu <1069950269@qq.com>
2018-11-07 11:04:37 +0800

    :cache

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -24,7 +24,7 @@ class Index extends Controller
             $token = input('param.token');
             $operat_man = input('param.operat_man');
             $card_type = input('param.card_type');
-            if(Cache::get('token','') == ""){
+            if(Cache::get('token') == ""){
                 return json('error','请先获取token!');
             }
             $key = md5($company_code.$num.$fvalue.Cache::get('token')[''.$operat_man]);
